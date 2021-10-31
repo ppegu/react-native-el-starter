fd87448dfcf936c7a0f69f35a9421880aa1bc255
Pran Pegu <pranpegu997@gmail.com>
2021-10-31 22:44:20 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App: React.ReactNode = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+export default App;
